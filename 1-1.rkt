8604bac217f0c8e947dd74f3a1798d259c9302de
peeyushaga <123811319+peeyushaga@users.noreply.github.com>
2023-08-01 03:45:10 +0000

    Initial commit

---
+++ 1-1.rkt
@@ -0,0 +1,33 @@
+#lang sicp
+
+;1.1.1
+
+10  ;10
+(+ 5 3 4)  ;12
+(- 9 1)  ;8
+(/ 6 2)  ;3
+(+ (* 2 4) (- 4 6))  ;6
+
+;1.1.2
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))  ;19
+(= a b)  ;False
+
+
+;1.3
+
+(define (square x) (* x x))
+
+(define (sum-square x y) (+ (square x) (square y)))
+
+(define (square-sum-larger x y z)
+  (cond ((and (<= x y) (<= x z)) (sum-square y z))
+        ((and (<= y x) (<= y z)) (sum-square x z))
+        ((and (<= z x) (<= z y)) (sum-square x z))))
+
+
+;1.4
+(+ a abs(b))
+(if(b>0) (+a b) (- a b))
+(+ a if(b>0) b (-b))
---
+++ README.md
@@ -0,0 +1,5 @@
+# Racket
+## the the Language-Oriented Programming Language
+### You can run app with run button or type in shell "racket main.rkt"
+
+# Official website for docs and more information: https://racket-lang.org/
